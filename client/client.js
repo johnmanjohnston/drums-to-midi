09ce18d93ece0c1e8f7cad93a091d244d84173cc
johnmanjohnston <c.ethanjohn@gmail.com>
2024-10-04 16:31:09 +0400

    better transient detection, added metronome for server

--- client/client.js
+++ client/client.js
@@ -1,6 +1,12 @@
 const socket = io();
 var titleElement = document.querySelector("#title");
 var micVolElement = document.querySelector("#mic-vol");
+var transientIndicatorEl = document.querySelector("#transient-indicator");
+
+var gateSlider = document.querySelector("#gate-slider");
+var gateDisplay = document.querySelector("#gate-display-text");
+
+var gate = 200;
 
 // var selectElement = document.querySelector("#drum");
 // var currentDrum = selectElement.value;
@@ -16,32 +22,41 @@ function displaySocketID() {
 
 displaySocketID();    
 
+function onGateSliderUpdate(value) {
+    gate = value;
+    gateDisplay.innerText = value;  
+}
 
 function startMic() {
-    alert("startmic called");
+    // alert("startmic called");
 
     navigator.mediaDevices.getUserMedia({audio: true, video: false}).then((stream) => {
-        alert(stream.active)
-
+        // alert(stream.active)
         const audioContext = new AudioContext();
         const analyser = audioContext.createAnalyser();
         const microphone = audioContext.createMediaStreamSource(stream);
         const scriptProcessor = audioContext.createScriptProcessor(2048, 1, 1);
 
-        analyser.smoothingTimeConstant = 0.8;
-        analyser.fftSize = 1024;
+        analyser.smoothingTimeConstant = 0;
+        analyser.fftSize = 2048;
 
         microphone.connect(analyser);
         analyser.connect(scriptProcessor);
         scriptProcessor.connect(audioContext.destination);
-        scriptProcessor.onaudioprocess = function() {
-        const array = new Uint8Array(analyser.frequencyBinCount);
-        analyser.getByteFrequencyData(array);
-        const arraySum = array.reduce((a, value) => a + value, 0);
-        const average = arraySum / array.length;
-        console.log(Math.round(average));
 
-        micVolElement.innerHTML = Math.round(average);      
-    };
-    });
+        scriptProcessor.onaudioprocess = function() {
+            const array = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteFrequencyData(array);
+            const arraySum = array.reduce((a, value) => a + value, 0);
+            const average = arraySum / array.length;
+            console.log(Math.round(average));
+
+            micVolElement.innerHTML = Math.round(arraySum / 100);   
+            
+            var loudness = Math.round(arraySum / 100);            
+            if (loudness > gate) {
+                transientIndicatorEl.innerHTML += "<div>transient indicated</div>";
+            }
+        };
+    }); 
 }
--- server/server.ts
+++ server/server.ts
@@ -3,16 +3,13 @@ import { createServer, RequestListener } from "node:http";
 import * as https from "node:https";
 import { join } from "node:path";
 import * as fs from "node:fs";
-import { Server, Socket } from "socket.io"
-
-// var key = fs.readFileSync(__dirname + '/../selfsigned.key');
-// var cert = fs.readFileSync(__dirname + '/../selfsigned.crt');
-
+import { Server, Socket } from "socket.io";
+    
 var key = fs.readFileSync(join(__dirname, "../selfsigned.key"));
 var cert = fs.readFileSync(join(__dirname, "../selfsigned.crt"));
 var options = {
-  key: key,
-  cert: cert
+    key: key,
+    cert: cert
 };
 
 const app = express();
@@ -20,7 +17,7 @@ const server = https.createServer(options, app);
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-res.sendFile(join(__dirname, "../client/", "index.html"));      
+    res.sendFile(join(__dirname, "../client/", "index.html"));      
 });     
 
 app.use("/asset", express.static(join(__dirname, "../client")));
@@ -37,6 +34,24 @@ io.on("connection", (socket: Socket) => {
     });     
 }); 
 
+var SONG_TEMPO: number = 100;
+var BAR = 1;
+var BEAT = 1    ; // in 4th notes   
+
+function metronome() {
+    console.log(`BEAT: ${BAR}.${BEAT}`);
+
+    BEAT++;
+    
+    if ((BEAT - 1) % 4 == 0) { 
+        BAR++;
+        BEAT = 1;
+    }
+
+    setTimeout(metronome, (60 / SONG_TEMPO) * 1000);
+}
+
 server.listen(8000, () => {
     console.log("server running on port 8000");
+    metronome();
 });
